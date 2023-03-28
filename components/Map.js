8567987a9456a9e9687163ce4bf0bff5136705b1
kolade2 <kolade@DESKTOP-O8TH9FH.localdomain>
2023-03-28 01:21:18 -0400

    trying to add buildling-violations to map

--- components/Map.js
+++ components/Map.js
@@ -46,10 +46,70 @@ const Map = () => {
           },
           'source-layer': 'census2020_tracts-baue3k',
           'paint': {
-             'fill-color': '#627BC1',
-              'fill-opacity': 0.5
+            'fill-color': 'blue',
+            'fill-opacity': 0.5,
           }
           });
+         
+          map.addSource('violations', {
+            type: 'geojson',
+            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+            data: '../data/building-violations.geojson',
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+          });
+          map.addLayer({
+            id: 'clusters',
+            type: 'circle',
+            source: 'violations',
+            filter: ['has', 'point_count'],
+            paint: {
+            'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                100,
+                '#f1f075',
+                750,
+                '#f28cb1'
+            ],
+            'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40
+            ]
+          }
+          });
+          map.addLayer({
+            id: 'cluster-count',
+            type: 'symbol',
+            source: 'violations',
+            filter: ['has', 'point_count'],
+            layout: {
+                'text-field': ['get', 'point_count_abbreviated'],
+                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                'text-size': 12
+              }
+            });
+            
+            map.addLayer({
+            id: 'unclustered-point',
+            type: 'circle',
+            source: 'violations',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+                'circle-color': '#11b4da',
+                'circle-radius': 4,
+                'circle-stroke-width': 1,
+                'circle-stroke-color': '#fff'
+            }
+            });
       });
     };
 
