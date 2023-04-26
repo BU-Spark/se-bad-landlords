1f99f90de63cec23afe8a1344ec2910673f31e15
kolade2 <kolade@DESKTOP-O8TH9FH.localdomain>
2023-04-26 15:31:18 -0400

    added all missing files

--- components/Map.js
+++ components/Map.js
@@ -1,28 +1,47 @@
+
 import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import React, { useEffect, useRef, useState } from "react";
+import Table from "./Table"; 
 
 const Map = () => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
-  
+  const [tableData, setTableData] = useState([]);
+  var hoveredStateId = ""; 
+  var neighborhood = "";
+  var census_tract_no = "";
+
   //===============================================
   //initialize map with access token and mapboxgl
   //===============================================
   useEffect(() => {
+    // Update table data with violations within map view bounds
     mapboxgl.accessToken = 'pk.eyJ1Ijoia29sYWRlYWRlZ2JheWUiLCJhIjoiY2xmcHNpMnBrMDFxNzQybzNzcHFrenI5OSJ9.pNixVb7d1UUfPIgmxqoitg';
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v11",
+        style: "mapbox://styles/mapbox/streets-v12",
         center: [-71.0589, 42.3601],
-        zoom: 11
+        zoom: 12
       });
-      let tract_id = null;
+      
+      // Add the control to the map.
+      map.addControl(
+          new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl
+        })
+      );
+      
+ 
+      
       //===============================================
       //load the default map mapbox/streets-v11
       //===============================================
       map.on('load', () => {
         setMap(map);
+
         
         //=================================================================
         //add census data information from mapbox studio to map as a source
@@ -31,8 +50,8 @@ const Map = () => {
         map.addSource('census', {
           type: 'vector',
           url: 'mapbox://koladeadegbaye.bpyqog0g'
-          });
-
+        });
+        
         //===================================================
         //add census data layer to map
         //source layer information should be the tileset name
@@ -40,169 +59,175 @@ const Map = () => {
         map.addLayer({
           'id': 'census-block-layer',
           'type': 'fill',
-           'source': {
+          'source': {
             'type': 'vector',
             'url': 'mapbox://koladeadegbaye.bpyqog0g'
           },
           'source-layer': 'census2020_tracts-baue3k',
-          'paint': {
+          'paint':{
             'fill-color': 'blue',
             'fill-outline-color': 'red',
-            'fill-opacity': 0.3,
+            'fill-opacity': 0.5
           }
-          });
-         
-          map.on('mousemove', 'census-block-layer', (f) =>{
-              tract_id = f.features[0].properties["FID"];
-              map.getCanvas().style.cursor = 'pointer';
-              map.setFeatureState(
-                {
-                  source: 'census',
-                  id: tract_id
-                },
-                {
-                  hover: true
-                }
-              );
-              map.setPaintProperty('census-block-layer', 'census', 'blue');
-          });
-          // When the mouse leaves the earthquakes-viz layer, update the
-          // feature state of the previously hovered feature
-          map.on('mouseleave', 'census-block-layer', () => {
-              if (tract_id) {
-                map.setFeatureState(
-              {
-                source: 'census',
-                id: tract_id
-              },
-              {
-                hover: false
+        });
+        // map.on('mousemove', 'census-block-layer', (e) => {
+        //   if (e.features.length > 0) {
+        //       hoveredStateId = e.features[0].id;
+        //       census_tract_no = e.features[0].properties["TRACTCE20"]
+        //     }
+
+        //   });
+
+    //NEIGHBORHOODS LAYER CODE
+    ///////////////////////////////////////////////////////////////////////////////////////
+        map.addSource('neighborhoods', {
+          type: 'vector',
+          url: 'mapbox://koladeadegbaye.331my0vd'
+        });
+
+        map.addLayer({
+            'id': 'neighborhoods-fills',
+            'type': 'fill',
+            'source': 'neighborhoods',
+            'source-layer': 'census2020_bg_neighborhoods-bttfiu',
+            'layout': {},
+            'paint': {
+            'fill-color': 'blue',
+            'fill-opacity': [
+            'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                0.5,
+                0
+                ]
               }
-              );
-              }
-              tract_id = null;
-              // Reset the cursor style
-              map.getCanvas().style.cursor = '';
           });
           
+          map.addLayer({
+              'id': 'neighborhoods-borders',
+              'type': 'line',
+              'source': 'neighborhoods',
+              'source-layer': 'census2020_bg_neighborhoods-bttfiu',
+              'layout': {},
+              'paint': {
+              'line-color': 'purple',
+              'line-width': 2
+            }
+          });
+          // When the user moves their mouse over the state-fill layer, we'll update the
+          // feature state for the feature under the mouse.
+          map.on('mousemove', 'neighborhoods-fills', (e) => {
+            if (e.features.length > 0) {
+              if (hoveredStateId !== null) {
+                map.setFeatureState(
+                  { source: 'neighborhoods', sourceLayer: 'census2020_bg_neighborhoods-bttfiu', id: hoveredStateId },
+                  { hover: false }
+                );
+              }
+              hoveredStateId = e.features[0].id;
+              neighborhood = e.features[0].properties["BlockGr202"]
+              map.setFeatureState(
+                { source: 'neighborhoods', sourceLayer: 'census2020_bg_neighborhoods-bttfiu', id: hoveredStateId },
+                { hover: true }
+              );
+            }
+          });
+
+          // When the mouse leaves the state-fill layer, update the feature state of the
+          // previously hovered feature and hide the tooltip.
+          map.on('mouseleave', 'neighborhoods-fills', () => {
+            if (hoveredStateId !== null) {
+              map.setFeatureState(
+                { source: 'neighborhoods', sourceLayer: 'census2020_bg_neighborhoods-bttfiu', id: hoveredStateId },
+                { hover: false }
+              );
+            }
+            hoveredStateId = null;
+          });
+
+/////////////////////////////violations code////////////////////////////////////////////////////////
           map.addSource('violations', {
             type: 'geojson',
-            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-            data: 'https://kolade2.github.io/Bad-Landlords/data/build-viol.geojson',
-            cluster: true,
+            data: 'https://kolade2.github.io/Bad-Landlords/data/updated_data.csv.geojson',
+            cluster: false,
             clusterMaxZoom: 14, // Max zoom to cluster points on
             clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
           });
-          
+
           map.addLayer({
-            id: 'clusters',
-            type: 'circle',
-            source: 'violations',
-            filter: ['has', 'point_count'],
-            paint: {
-            'circle-color': [
-                'step',
-                ['get', 'point_count'],
-                '#31C1F7',
-                90,
-                '#A7E7FF',
-                650,
-                '#C0A9FD'
-            ],
-            'circle-radius': [
-                'step',
-                ['get', 'point_count'],
-                10,
-                90,
-                20,
-                650,
-                30
-            ]
+          id: 'unclustered-point',
+          type: 'circle',
+          source: 'violations',
+          filter: ['!', ['has', 'point_count']],
+          paint: {
+              'circle-color': '#11b4da',
+              'circle-radius': 4,
+              'circle-stroke-width': 2,
+              'circle-stroke-color': '#fff'
           }
           });
-          map.addLayer({
-            id: 'cluster-count',
-            type: 'symbol',
-            source: 'violations',
-            filter: ['has', 'point_count'],
-            layout: {
-                'text-field': ['get', 'point_count_abbreviated'],
-                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-                'text-size': 11
+          
+          var popupContainer = document.getElementById('popupContainer');
+
+          map.on('click', 'unclustered-point', (e) => {
+            const filteredProperties = {};
+            const properties = e.features[0].properties;
+            for (const key in properties) {
+              if (properties.hasOwnProperty(key) && properties[key] !== "") {
+                filteredProperties[key] = properties[key];
               }
-            });
-            
-            map.addLayer({
-            id: 'unclustered-point',
-            type: 'circle',
-            source: 'violations',
-            filter: ['!', ['has', 'point_count']],
-            paint: {
-                'circle-color': '#11b4da',
-                'circle-radius': 4,
-                'circle-stroke-width': 1,
-                'circle-stroke-color': '#fff'
             }
-            });
+            const { code, OWNER1, description, case_no } = filteredProperties;
+            const coordinates = e.features[0].geometry.coordinates.slice();
             
-            // inspect a cluster on click
-            map.on('click', 'clusters', (e) => {
-                const features = map.queryRenderedFeatures(e.point, {
-                layers: ['clusters']
-            });
-            
-            const clusterId = features[0].properties.cluster_id;
-            map.getSource('violations').getClusterExpansionZoom(
-            clusterId,
-            (err, zoom) => {
-            if (err) return;
-                map.easeTo({
-                center: features[0].geometry.coordinates,
-                zoom: zoom
-                });
+              // Update the contents of the popup container
+            popupContainer.innerHTML = `
+                <div><strong>Neighborhood:</strong></div>${neighborhood}</div>
+                <div><strong>Name:</strong> ${OWNER1}</div>
+                <div><strong>Code:</strong> ${code}</div>
+                <div><strong>Description:</strong> ${description}</div>
+                <div><strong>Case No:</strong> ${case_no}</div>
+                `;
+              // Show the popup container
+            popupContainer.style.display = 'block';
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                  coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                 }
-            );
-            });
-            
-            // When a click event occurs on a feature in
-            // the unclustered-point layer, open a popup at
-            // the location of the feature, with
-            // description HTML from its properties.
-            map.on('click', 'unclustered-point', (e) => {
-                const coordinates = e.features[0].geometry.coordinates.slice();
-                const code = e.features[1].properties["code"];
-                const addr = e.features[1].properties["contact_addr1"];
-                const description = e.features[1].properties["description"];
-                const date_time = e.features[1].properties["status_dttm"]
-                // Ensure that if the map is zoomed out such that
-                // multiple copies of the feature are visible, the
-                // popup appears over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
-                
-            new mapboxgl.Popup()
-              .setLngLat(coordinates)
-              .setHTML(
-              `Address: ${addr}<br> Violation code: ${code}<br> Description: ${description}<br> Date/Time: ${date_time}`
-            )
-              .addTo(map);
-            });
-            
-            map.on('mouseenter', 'clusters', () => {
-            map.getCanvas().style.cursor = 'pointer';
-            });
-            map.on('mouseleave', 'clusters', () => {
-            map.getCanvas().style.cursor = '';
-            });
+          });
+          
+          map.on('move', ['unclustered-point', 'neighborhood-fills'], (e) =>{
+              const bounds = map.getBounds();
+              const features = map.queryRenderedFeatures({ bounds });
+              const violationsData = features.map(e => ({
+                id: e.properties.OID_, // or any other unique identifier for the row
+                neighborhood: neighborhood,
+                owner: e.properties.OWNER1, // replace with actual property name
+                code: e.properties.code, // replace with actual property name
+                description: e.properties.description,
+                case_no: e.properties.case_no
+                // add more properties as needed
+                }));
+                console.log(violationsData);
+                setTableData(violationsData);
+          })
+           
       }); 
     };
 
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
-
-  return <div className="map-container" ref={mapContainer} />;
+  return (
+    <div>
+      <div ref={mapContainer} style={{Top: 300, height:550, width:'100%'}}/>
+      <br></br>
+      <h2 className ="table-title"> Building violations table</h2>
+      <p className="table-text">Datapoints from the Map above displayed on a table</p>
+      <div className="Table">
+           <Table data={tableData}/>
+      </div>
+     
+    </div>
+  );
 };
 
 export default Map;
+
