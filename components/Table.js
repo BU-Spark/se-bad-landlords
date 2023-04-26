89a015d2dbecd2682e503923813b4b77c80cb3ea
kolade2 <kolade@DESKTOP-O8TH9FH.localdomain>
2023-04-26 15:05:32 -0400

    added new files

---
+++ components/Table.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { DataGrid, GridToolbar} from '@mui/x-data-grid';
+
+const Table = ({ data }) => {
+  const [rows, setRows] = useState([]);
+  const [page, setPage] = useState(0);
+
+  // Calculate the number of rows to display per page
+  const rowsPerPage = 25;
+
+  // Update the rows state based on the current page
+  useEffect(() => {
+    const startIndex = page * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    const visibleRows = data.slice(startIndex, endIndex);
+    setRows(visibleRows);
+  }, [data, page]);
+
+  const handlePageChange = (params) => {
+    setPage(params.page);
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'OwnerID', width:100},
+    { field: 'owner', headerName: 'Landlord', width: 250 },
+    { field: 'neighborhood', headerName: 'Neighborhood', width: 150 },
+    { field: 'code', headerName: 'Code', width: 150 },
+    {
+      field: 'case_no',
+      headerName: 'case_number',
+      type: 'number',
+      width: 150,
+    },
+    { field: 'description', headerName: 'Violation description', width: 450 }
+  ];
+
+  return (
+    <div id="data-table" style={{width: 'auto', height: '60vh'}}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+      slots={{
+          toolbar: GridToolbar,
+        }}
+      
+     />
+    </div>
+  );
+};
+
+export default Table;
+
