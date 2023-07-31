a370f44a3c7b3e4fba79535d0456983795690cc4
Jason Oh <jasonoh1998@gmail.com>
2023-07-30 20:33:26 -0400

    update bpv script

--- database_scripts/bpvSetup.py
+++ database_scripts/bpvSetup.py
@@ -1,10 +1,18 @@
+# Total elapsed time: 36.67883014678955 seconds
+# bpvSetup script updates the existing table
+# if you need the table to be deleted use the commented at line 33
+import time
 import requests
-from sqlalchemy import Inspector, create_engine, MetaData, Table, Column, Text, Integer, exc
+from sqlalchemy import create_engine, MetaData, Table, Column, Text, Integer, exc
+from sqlalchemy.dialects.postgresql import insert
 
-# Script deletes the current BPV table and replace it with a new one
-# Currently storing 14382 rows everytime it runs (2023-07-13)
+# constants
+API_URL = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=800a2663-1d6a-46e7-9356-bedb70f5332c'
+BASE_API_URL = 'https://data.boston.gov'
+DB = "postgresql://postgres:postgres@localhost:5432/badlandlords"
+
 def json_to_table(api, table_name):
-    engine = create_engine("postgresql://postgres:postgres@localhost:5432/badlandlords")
+    engine = create_engine(DB)
     metadata = MetaData()
 
     # fetch data from api link
@@ -18,17 +26,22 @@ def json_to_table(api, table_name):
         'int': Integer,
     }
 
-    # drop table
-    if Inspector.has_table(table_name):
-        table = Table(table_name, metadata, autoload_with=engine)
-        table.drop(engine)
-    metadata.clear()
+    # specify the headers you will like to pull
+    desired_headers = ['case_no', "status_dttm", "status", "code", "description", "sam_id", "latitude", "longitude"]
+
+    # use this if you want to drop the table and create a fresh one
+    # try:
+    #     table = Table(table_name, metadata, autoload_with=engine)
+    #     table.drop(engine)
+    # except exc.NoSuchTableError:
+    #     pass
+    # metadata.clear()
 
     # create the table
     table = Table(
         table_name, 
-        metadata, 
-        *[Column(header['id'], types[header['type']]) for header in headers]
+        metadata,
+        *[Column(header['id'], types[header['type']], primary_key=(header['id'] == 'case_no')) for header in headers if header['id'] in desired_headers]
     )
 
     metadata.create_all(engine)
@@ -41,12 +54,16 @@ def json_to_table(api, table_name):
             for record in data['result']['records']:
                 # test case to see record data
                 # print(record)
-                ins = table.insert().values(**record)
+                record = {header: val for header, val in record.items() if header in desired_headers}
+                ins = insert(table).values(record).on_conflict_do_update(
+                    index_elements=['case_no'],
+                    set_=record
+                )
                 conn.execute(ins)
-
             # this will look for next page link but break if current page has empty records
+            # comment this out and the while loop to test first 100 values
             if '_links' in data['result'] and 'next' in data['result']['_links'] and data['result']['records']:
-                response = requests.get('https://data.boston.gov' + data['result']['_links']['next'])
+                response = requests.get(BASE_API_URL + data['result']['_links']['next'])
                 data = response.json()
             else:
                 break
@@ -56,9 +73,14 @@ def json_to_table(api, table_name):
         raise
 
 def main():
-    api = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=800a2663-1d6a-46e7-9356-bedb70f5332c'
+    start_time = time.time()
+
     table_name = 'bpv'
-    json_to_table(api, table_name)
+    json_to_table(API_URL, table_name)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Total elapsed time: {elapsed_time} seconds')
 
 if __name__ == "__main__":
     main()
