48d2f65071bc1b1fa6a26c5ff228e90a99605349
christorange <christol.yu@outlook.com>
2023-08-02 13:08:17 -0400

    some deletions

--- pages/mapPage.js
+++ pages/mapPage.js
@@ -12,12 +12,7 @@ const MapPage = () => {
             <div className="section-title">
                 <h2 className="section-title-header">Bad Landlords Visualization</h2>
             </div>
-            <p className="section-header">Buildling violations in Boston neighborhoods</p>
             <hr className="hr-index"></hr> 
-            {/* <div className="w-full mx-0 my-auto">
-                <Map />
-            </div>
-            <p className='mt-8 mb-5 text-2xl'>New map</p> */}
             <NewMap />
             <div id="popupContainer">
             </div>
