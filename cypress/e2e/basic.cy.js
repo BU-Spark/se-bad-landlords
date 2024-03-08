bbb9d941f0588cd9f6a8a4583110f9b4740d629e
Zhaozhan <nonox530042@gmail.com>
2024-03-07 20:50:42 -0500

    set up 'npm start' and 'npm run test'. Sample test in Cypress.

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,15 +3,18 @@ describe('sample test', () => {
     cy.visit('/')
   })
 
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Welcome to my app!');
-  })
-  it('renders the Netlify logo image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+  // it('displays the resources text', () => {
+  //   cy.get('h1')
+  //   .contains('Welcome to my app!');
+  // })
+  // it('renders the Netlify logo image', () => {
+  //   cy.get('img')
+  //   .should('be.visible')
+  //   .and(($img) => {
+  //     expect($img[0].naturalWidth).to.be.greaterThan(0);
+  //   })
+  // })
+  it("display 'HOME' text in main page", () => {
+    cy.contains("div", "HOME").should("be.visible");
   })
 })
